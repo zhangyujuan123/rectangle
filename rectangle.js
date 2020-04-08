5a9c810835b6aa07e879cefb77ad163994a5fe88
zhangyujuan123 <zyj15231108763@163.com>
2020-04-08 22:37:41 +0800

    add

--- rectangle.js
+++ rectangle.js
@@ -1,64 +1,60 @@
-$(function(){
-    //get dom elem
-    var $width=$('#width'),
-        $height=$('#height'),
-        $btncal=$('#calculate'),
-        $perimeter=$('#perimeter'),
-        $area=$('#area');
-    function roundFractional(x, n) {
-        return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
+/* global Rectangle, validate, isLegalKey, $forkMeGH, $bszPageFooter: true */
+$(function() {
+  var $width = $('#width'),
+      $height = $('#height'),
+      $btnCal = $('#calculate'),
+      $perimeter = $('#perimeter'),
+      $area = $('#area'),
+      $widthValidate = $('#width-validate'),
+      $heightValidate = $('#height-validate'),
+      isPassValidate = false;
+
+  $forkMeGH.show('https://github.com/wangding/rectangle');
+  $bszPageFooter.show('body');
+
+  $width.focusout(function() {
+    var result = validate($width.val());
+    isPassValidate = result.isOK;
+    if(!result.isOK) {
+      $widthValidate.html('宽度' + result.reason);
+      $width.select();
+    } else {
+      $widthValidate.html('');
     }
-    /**calc button click event */
-    $btncal.click(function(){
-        //validate if error return;
-        if(!validate('#width')||!validate('#height')) return;
+  });
 
-
-        //get value
-        var w=Number($width.val()),
-            h=Number($height.val());
-        //calculate
-        var p=roundFractional(2*(w+h),1),
-            a=roundFractional(w*h,2);
-        //output
-        $perimeter.val(p);
-        $area.val(a);
-    });
-    $width.focusout(function(){
-        //if(!validate('#wdith')) select this;
-        if(!validate('#width')) $width.select();
-    });
-    $height.focusout(function(){
-        //if(!validate('#height')) select this;
-        if(!validate('#height')) $height.select();
-    });
-
-
-
-    function validate(field){
-        //get DOM error message
-        var $data=$(field),
-            $message=$(field+'-validation-message');
-        //validate null
-        if($data.val()===''){
-            $message.html('不能为空！');
-            $data.select();
-            return false;
-        }
-        //validate number
-        if(!(/^-?(0|[1-9]\d*)(\.\d*)?([eE][+-]?\d+)?$/.test($data.val()))){
-            $message.html('必须是数值！');
-            $data.select();
-            return false;
-        }
-        //validate>0
-        if(Number($data.val()) < 0){
-            $message.html('必须大于零！');
-            $data.select();
-            return false;
-        }
-        $message.html(' ');
-        return true;
+  $width.keypress(function(e) {
+    if(!isLegalKey(e.key, e.target.value, e.target.selectionStart)) {
+      e.preventDefault();
     }
+  });
 
+  $height.focusout(function() {
+    var result = validate($height.val());
+    isPassValidate = result.isOK;
+    if(!result.isOK) {
+      $heightValidate.html('高度' + result.reason);
+      $height.select();
+    } else {
+      $heightValidate.html('');
+    }
+  });
+
+  $height.keypress(function(e) {
+    if(!isLegalKey(e.key, e.target.value, e.target.selectionStart)) {
+      e.preventDefault();
+    }
+  });
+
+  $btnCal.click(function(){
+    if(!isPassValidate) return;
+
+    var w = $width.val(),
+        h = $height.val();
+
+    var r = new Rectangle(w, h);
+
+    $perimeter.val(r.perimeter());
+    $area.val(r.area());
+  });
 });
